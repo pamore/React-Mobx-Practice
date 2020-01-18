189db73261b8a6de5dcd753240ec7fb999e23c9a
prachi.more1 <prachi.more@integral.io>
2020-01-18 15:30:46 -0500

    Use Observable, computed, autorun to automatically print log on state change.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {TodoStore} from './TodoStore';
 
 function App() {
+  let todoStore = new TodoStore();
+  todoStore.addTodo("Task 1");
+  todoStore.addTodo("Task 2");
+  todoStore.todoList[0].completed = true;
+  todoStore.todoList[1].task = "Task 2a";
+  todoStore.todoList[0].task = "Task 1a";
+
   return (
     <div className="App">
-      
+      Running...{todoStore.todoList.length}
     </div>
   );
 }
---
+++ src/TodoStore.js
@@ -0,0 +1,43 @@
+import {autorun, computed, decorate, observable} from "mobx";
+
+export class TodoStore {
+    todoList = [];
+
+    constructor() {
+        autorun(() => {
+            console.log(this.report);
+        });
+    }
+    get completedCount() {
+        return this.todoList.filter(
+            todo => todo.completed === true
+        ).length;
+    }
+
+    get report() {
+        if (this.todoList.length === 0) {
+            return "<none>";
+        }
+        let nextTodo = this.todoList.find(
+            todo => todo.completed === false
+        );
+        let result = "Next TODO: "
+            + (nextTodo ? nextTodo.task : null)
+            + "    "
+            + "Progress: " + this.completedCount + "/" + this.todoList.length;
+        // console.log(result);
+        return result ;
+    }
+    addTodo(task){
+        this.todoList.push({
+            task: task,
+            completed: false,
+            assignee: null
+        })
+    }
+}
+decorate(TodoStore, {
+    todoList: observable,
+    completedCount: computed,
+    report: computed
+});
