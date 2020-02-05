7cfd02e9bf8cf6883b317957f96075663132b8d5
prachi.more1 <prachi.more@integral.io>
2020-02-04 20:31:03 -0500

    Create list for the time travel buttons.

--- src/TicTacToeGame/TimeTravelSection.js
+++ src/TicTacToeGame/TimeTravelSection.js
@@ -1,5 +1,4 @@
 import React from "react";
-import './TimeTravelSection.css';
 
 export class TimeTravelSection extends React.Component {
 
@@ -9,20 +8,23 @@ export class TimeTravelSection extends React.Component {
         buttons.push(
             this.getButton(0)
         );
-        for(i = 1; i < this.props.moves; i++) {
+        for (i = 1; i < this.props.moves; i++) {
             buttons.push(this.getButton(i));
         }
         return buttons;
     }
 
     getButton(i) {
-        return <button
-            key={i}
-            className="timeTravelButton"
-            onClick={() => (this.props.onClickButton(i))}
-        >
-            {i===0 ? "RestartGame": "Go to move #"+i}
-        </button>;
+        return (
+            <li key={i}>
+                <button
+                    className="timeTravelButton"
+                    onClick={() => (this.props.onClickButton(i))}
+                >
+                    {i === 0 ? "RestartGame" : "Go to move #" + i}
+                </button>
+            </li>
+        );
     }
 
     render() {
