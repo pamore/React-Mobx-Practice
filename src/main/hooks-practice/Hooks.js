49aa06935f3f42761554475cf203649abdf61224
prachi.more1 <prachi.more@integral.io>
2020-02-22 18:11:53 -0500

    UPdated state hook implementation.

--- src/main/hooks-practice/Hooks.js
+++ src/main/hooks-practice/Hooks.js
@@ -1,54 +1,47 @@
-import React from 'react';
-import {useState} from "react";
+import React, {useState} from 'react';
 import './Hooks.css';
 
 export function Hooks() {
     const [car, setCar] = useState({
-        type : "",
-        model : "",
-        year : ""
+        Brand: "",
+        Model: "",
+        Year: ""
     });
+
+    const getSelectOptions = ((property) =>
+            <select value={car[property]} onChange={(event) => setCar({...car, [property]: event.target.value})}>
+                <option value="">-Select-</option>
+                <option value={property + " 1"}>{property + " 1"}</option>
+                <option value={property + " 2"}>{property + " 2"}</option>
+                <option value={property + " 3"}>{property + " 3"}</option>
+                <option value={property + " 4"}>{property + " 4"}</option>
+                <option value={property + " 5"}>{property + " 5"}</option>
+            </select>
+    );
+
+    let submitButton = car.Brand !== "" && car.Model !== "" && car.Year !== "" && (<button onClick={() => alert("clicked")}>Submit</button>);
+
     return (
         <div>
             <h2>Hooks - </h2>
             <div>
                 <label className="selectOption">
                     Car : &nbsp;
-                    <select value={car.type} onChange={(event) => setCar({...car, type : event.target.value})}>
-                        <option value="">Select Car</option>
-                        <option value="Car 1">Car 1</option>
-                        <option value="Car 2">Car 2</option>
-                        <option value="Car 3">Car 3</option>
-                        <option value="Car 4">Car 4</option>
-                        <option value="Car 5">Car 5</option>
-                    </select>
+                    {getSelectOptions("Brand")}
                 </label>
                 <label className="selectOption">
                     Model : &nbsp;
-                    <select value={car.model} onChange={(event) => setCar({...car, model : event.target.value})}>
-                        <option value="">Select Model</option>
-                        <option value="Mode 1">Model 1</option>
-                        <option value="Model 2">Model 2</option>
-                        <option value="Model 3">Model 3</option>
-                        <option value="Model 4">Model 4</option>
-                        <option value="Model 5">Model 5</option>
-                    </select>
+                    {getSelectOptions("Model")}
                 </label>
-                <label  className="selectOption">
+                <label className="selectOption">
                     Year : &nbsp;
-                    <select value={car.year} onChange={(event) => setCar({...car, year : event.target.value})}>
-                        <option value="">Select Year</option>
-                        <option value="2017">2017</option>
-                        <option value="2018">2018</option>
-                        <option value="2019">2019</option>
-                        <option value="2020">2020</option>
-                        <option value="2021">2021</option>
-                    </select>
+                    {getSelectOptions("Year")}
                 </label>
                 <br/>
-                <label className="hookResult">Result : {car.type} - {car.model}  - {car.year}</label>
+                <label className="hookResult">Result : {car.Brand} - {car.Model} - {car.Year}</label>
                 <br/>
-                { car.type!=="" && car.model!=="" && car.year!=="" && (<button onClick={() => alert("clicked")}>Submit</button>)}
+                {submitButton
+                }
             </div>
         </div>
     );
